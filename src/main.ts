ab31b5add520564b7a0d546162e67504d241e0f3
André Nunes <hello@anuness.dev>
2025-11-20 22:59:31 +0000

    feat(swagger): add swagger for better documentation and testing

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('BlueOtter API')
+    .setDescription(
+      'Essa api permite interagir e manipular dados de utilizadores do GitHub',
+    )
+    .setVersion('1.0.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
